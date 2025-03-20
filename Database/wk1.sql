4aff8500f126ccf7bfe6b5b66e6a816b5f72db8a
nmwarish <nanimwarish@gmail.com>
2025-03-20 13:19:50 +0300

    Added Week 1 assignments: w1.html and wk1.sql

---
+++ Database/wk1.sql
@@ -0,0 +1,12 @@
+--  Assignment Questions
+-- Write an SQL query to create a new database called salesDB.
+
+
+create database salesDB;
+
+
+-- Write an SQL query to drop (delete) the database called demo.
+drop database demo;
+
+
+
